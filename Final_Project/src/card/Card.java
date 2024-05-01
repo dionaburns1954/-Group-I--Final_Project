a0e77fd3ea9446efdebd1f379af2ea8c253e98ea
dionb <dionb@Mogira>
2024-05-01 11:32:36 -0500

    Added a skeleton to Card and Deck classes

---
+++ Final_Project/src/card/Card.java
@@ -0,0 +1,25 @@
+
+public class Card {
+	
+	private int value;
+	
+	
+	public Card() { //default constructor
+		this.value = 1;
+	}
+	
+	public Card(int value) { //parametized constructor
+		this.value = value;
+	}
+	
+	private void setValue(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	
+	
+}
---
+++ Final_Project/src/deck/Deck.java
@@ -0,0 +1,37 @@
+
+public class Deck {
+	
+	ArrayList<Card> deck;
+	
+	
+	public Deck() {
+		this.deck = new ArrayList<Card>();
+	}
+	
+	public boolean pullCardsFromDeck(int num) {
+		/*
+		 * function takes in a num of cards representing
+		 * how many cards to pull from top of deck.
+		 * Method pulls num of cards from top of deck and deletes them
+		 * from arraylist. Method returns truee if successful, false if unsuccessful
+		 */
+		
+		return false;
+	}
+	
+	private boolean deleteCardFromDeck(Card Card) {
+		//removes a certain card from the deck
+		//card that is to be removed are ones that have been pulled
+		//method returns true if card was successfully removed from deck, else returns false
+		
+		return false;
+	}
+	
+	public boolean shuffleDeck() {
+		//shuffles deck of cards before playing
+		//if deck is shuffled correctly, return true, else return false
+		
+		return false; 
+	}
+	
+}
