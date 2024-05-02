623b9eee966d3f969e971e446d976315cdd37f77
RedPh <lmb7gq@umsystem.edu>
2024-05-01 19:22:07 -0500

    shop fixed up //added open shop menu method

--- src/edu/mu/shop/shop.java
+++ src/edu/mu/shop/shop.java
@@ -6,49 +6,120 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class shop {
-	/*DELETE THIS LINE TO UNCOMMENT
-	 * 
-	 * private List<Item> allItems; private List<Item> displayedItems; private int
-	 * currency;
-	 * 
-	 * public shop() { allItems= new ArrayList<>(); displayedItems = new
-	 * ArrayList<>(); currency = 0; Initialize_All_Items();
-	 * Select_Displayed_Items(); }//test
-	 * 
-	 * private void Initialize_All_Items() { // ADD ANY ITEMS HERE ADD TO ALL ITEMS
-	 * ARRAY } private void Select_Displayed_Items() { displayedItems.clear(); //
-	 * clear existing display of items
-	 * 
-	 * 
-	 * displayedItems.add(Get_Random_Single_Card()); displayedItems.add(new
-	 * DeleteCardItem( what ever delete card ) ; displayedItems.add( new
-	 * ShuffleShopItem( what ever shuffle shop);
-	 * displayedItems.add(Get_Random_BoosterPack()); for ( int i = 0; i < 3; i ++)
-	 * {// for how ever other items we would want to add
-	 * displayedItems.add(Get_Random_Item()); } } private Item
-	 * Get_Random_BoosterPack() { Random random = new Random(); int chance =
-	 * random.nextInt(10);// generate random number between 0 and 9 if ( chance <
-	 * some value ) { return new boosterpack ( BRONZE); // repeat for other
-	 * boostpack values make it where it is garenteed 1 like if everythign fails
-	 * then hit this type thing } } private Item Get_Random_Single_Card() { Random
-	 * random = new Random(); int value = random.nextInt(10)+1; // generate random
-	 * value between 1 and 10 return new SingleCard( whate ver it is assign Value
-	 * here ); } private Item Get_Random_Item() { Random random = new Ramom();
-	 * return allItems.get(random.nextInt(allItems.size()));
-	 * 
-	 * } public void Open_Shop_Menu() {
-	 * 
-	 * } private void Display_Items () { for (int i =0; i< displayedItems.size();
-	 * i+) { Item item = displayedItems.get(i); system.out.println((i+1)+". " +
-	 * item.getName() + " - Price: $" + item.getPrice()); }
-	 * 
-	 * } private void Purchase_Item (Item item) { item.applyEffect(); currency -=
-	 * item.getPrice(); displayedItems.remove(item);
-	 * System.out.println("You Purchased: " + item.getName());
-	 * System.out.println("remaining Currency: $" + currency);
-	 * System.out.println("remaining Items in shop"); Display_Items();
-	 * 
-	 * } public void Add_Currency(int amount) { currency += amount; }
-	 * 
-	 *///DELETE THIS LINE TO UNCOMMENT
+
+	  
+	private List<Item> allItems; 
+	private List<Item> displayedItems; 
+	private int currency;
+	  
+	  
+	  
+	public shop() { 
+		allItems= new ArrayList<>(); 
+		displayedItems = new ArrayList<>();
+		currency = 0;
+		Initialize_All_Items();
+	    Select_Displayed_Items();
+	    }//test
+	  
+	private void Initialize_All_Items() { // ADD ANY ITEMS HERE ADD TO ALL ITEMS
+	
+	}
+	private void Select_Displayed_Items() {
+		displayedItems.clear();
+	  
+	  displayedItems.add(Get_Random_Single_Card());
+	  displayedItems.add(/*new DeleteCardItem( what ever delete card */);
+	  displayedItems.add(/* new ShuffleShopItem( what ever shuffle shop*/);
+	  displayedItems.add(Get_Random_BoosterPack());
+	  for ( int i = 0; i < 3; i ++) {// for how ever other items we would want to add
+		  displayedItems.add(Get_Random_Item());
+		  } 
+	  }
+	
+	private Item Get_Random_BoosterPack() {
+		Random random = new Random();
+		int chance =random.nextInt(10);// generate random number between 0 and 9 
+		if ( chance < /*some value */){ 
+			return new boosterpack ( BRONZE); // repeat for other boosterpack values make it where it is guaranteed 1 like if everything fails then hit this type thing 
+		} 
+	} 
+	
+	private Item Get_Random_Single_Card() { 
+		Random random = new Random();
+		int value = random.nextInt(10)+1; // generate random value between 1 and 10 
+	  return new SingleCard( /* whatever it is assign Value here */ );
+	   }
+	
+	private Item Get_Random_Item() {
+		Random random = new Random();
+	  return allItems.get(random.nextInt(allItems.size()));
+	  
+	  } 
+	
+	public void Open_Shop_Menu() {
+		Scanner scanner = new Scanner(System.in);
+		boolean exitShop = false;
+		
+		while ( !exitShop) {
+			System.out.println("Welcome to the Shop!");
+			Select_Displayed_Items();
+			System.out.println(" Your Currency: $" + currency);
+			System.out.println("Available Items:");
+			Display_Items();
+			
+			boolean validInput = false;
+					while (!validInput) {
+						System.out.println("Enter the number of the item you wish to purchase OR 'exit' to leave the shop:");
+						
+						String input = scanner.next();
+						
+						if ( input.equalsIgnoreCase("exit")) {
+							System.out.println("Exiting the Shop");
+							exitShop = true;
+							validInput = true;
+						} else {
+							try {
+								int itemNumber = Integer.parseInt(input);
+								if ( itemNumber >= 0 && itemNumber < displayedItems.size()) {
+									Item selectedItem = displayedItems.get(itemNumber);
+									if ( currency >= selectedItem.getPrice()) {
+										purchaseItem(selectedItem);
+										
+										
+										validInput = true;
+									}else {
+										System.out.println("Insufficient funds!");
+									}
+								} else {
+									System.out.println("invalid item number");
+								}
+							} catch ( NumberFormatException e) {
+								System.out.println("Invalid input, please enter a valid item number or exit to leave");
+					}
+				}
+			}
+		}
+	}
+	
+	private void Display_Items () {
+		for (int i =0; i< displayedItems.size(); i++) { 
+			Item item = displayedItems.get(i);
+			System.out.println((i+1)+". " +item.getName() + " - Price: $" + item.getPrice());
+			}
+	  }
+	private void Purchase_Item (Item item) { 
+		item.applyEffect();
+		currency -= item.getPrice();
+		displayedItems.remove(item);
+	  System.out.println("You Purchased: " + item.getName());
+	  System.out.println("remaining Currency: $" + currency);
+	  System.out.println("remaining Items in shop"); Display_Items();
+	  
+	  }
+	public void Add_Currency(int amount) { 
+		currency += amount;
+		}
+	  
+	 
 }
