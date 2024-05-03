87e890951e19009ece3812ff7c2c9c868ba82d86
Ryan Esparza <rte0315@gmail.com>
2024-05-02 23:17:25 -0500

    Added logic to select which card to delete

--- src/edu/mu/item/DeleteCardItem.java
+++ src/edu/mu/item/DeleteCardItem.java
@@ -1,6 +1,8 @@
 package edu.mu.item;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 import edu.mu.deck.Deck;
+import edu.mu.card.Card;
 
 public class DeleteCardItem extends Item{
 	private Deck deck;
@@ -11,7 +13,20 @@ public class DeleteCardItem extends Item{
 	
 	@Override
 	public void applyEffect() {
-		deck.deleteCard(null);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Select the card you want to delete:");
+		
+		ArrayList<Card> cards = deck.getCards();
+		for (int i = 0; i < cards.size(); i++) {
+			System.out.println((i + 1) + ". " + cards.get(i));
+		}
+		int choice = scanner.nextInt();
+		if (choice >= 1 && choice <= cards.size()) {
+			deck.deleteCard(cards.get(choice - 1));
+		} else {
+			System.out.println("Invalid choice. No card was deleted");
+		}
+		scanner.close();
 	}
 
 }
