4799dd7461bf8607b9d2e0d95ace04980349ab57
RedPh <lmb7gq@umsystem.edu>
2024-05-05 02:17:06 -0500

    edited UserDeck so that way the Deck class can be the one to create the list of cards, add, delete methods

--- src/edu/mu/deck/UserDeck.java
+++ src/edu/mu/deck/UserDeck.java
@@ -6,38 +6,23 @@ import edu.mu.card.*;
 
 
 public class UserDeck extends Deck {
-	private List<Card> initialCards;
+	
 	
 	public UserDeck() {
 		super();
-		initialCards = new ArrayList<>();
-		Int_intCards();
-		resetDeck();
+		intializeCards();
+	
 
 	}
 	
-	private void Int_intCards() {
-		initialCards.add(new Card(1));
-		initialCards.add(new Card(2));
-		initialCards.add(new Card(3));
-		initialCards.add(new Card(4));
-		initialCards.add(new Card(5));
+	private void intializeCards() {
+		for(int i =1;i<=5; i++) {
+			cards.add(new Card(i));
+		}
 	}
 	
 	private void resetDeck() { // initilizes deck as well as reset deck to begining deck 
-		clearDeck();
-		
-		for (Card card : initialCards) {
-			addCard(card);
-		}
+		cards.clear();
+		intializeCards();
 	}
-	public void addCardToDeck(Card card) {
-		addCard(card);
-	}
-	
-	public void deleteCardFromDeck(Card card) {
-		deleteCard(card);
-	}
-	
-	// maybe add shuffle 
 }
