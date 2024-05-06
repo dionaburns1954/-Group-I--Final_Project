b163a7ac0299e51570e5ab13737dfbfcfc5a47ad
Ryan Esparza <rte0315@gmail.com>
2024-05-06 00:47:26 -0500

    Wrote Javadoc for GameManager

--- src/edu/mu/gamemanager/GameManager.java
+++ src/edu/mu/gamemanager/GameManager.java
@@ -8,6 +8,14 @@ import edu.mu.players.PlayerOne;
 import edu.mu.players.PlayerTwo;
 import edu.mu.shop.shop;
 
+/**
+ * Manages the flow of the game, like starting battles, managing player health and levels,
+ * displaying player hands, and interacting with the shop
+ * 
+ * @author Dion Burns
+ * @author Logan Bird
+ * @author Ryan Esparza
+ */
 public class GameManager {
 	
 	private static GameManager instance;
@@ -18,11 +26,20 @@ public class GameManager {
 	//private final Deck deck;
 	private final int handNum = 7;
 	
+	/**
+	 * Private constructor for the GameManager
+	 * Initializes the player and enemy characters
+	 */
 	public GameManager() {
 		enemy = new PlayerTwo(enemyLvl);
 		//deck = new Deck();
 	}
 	
+	/**
+	 * Gives both the player and enemy a fresh hand of cards
+	 * 
+	 * @return True if both players have a non-empty hand after drawing cards, false otherwise
+	 */
 	public boolean giveHand() { //gives player and enemy a fresh hand. Supposed to be called at the beginning
 		player.setHand(player.getDeck().pullCardsFromDeck(handNum));
 		if(player.getHand().size() <= 0) return false;
@@ -35,22 +52,42 @@ public class GameManager {
 		
 	}
 	
+	/**
+	 * Retrieves the player's deck
+	 * 
+	 * @return The player's deck
+	 */
 	public Deck getPlayerDeck() {
 		return player.getDeck();
 	}
 	
+	/**
+	 * Checks if player one's health is greater than zero
+	 * 
+	 * @return True if player one's health is greater than zero, false otherwise
+	 */
 	private boolean checkPlayerOneHealth() {
 		if(player.checkPlayerHealth()) return true;
 		
 		return false;
 	}
 	
+	/**
+	 * Checks if player two's health is greater than zero
+	 * 
+	 * @return True if player two's health is greater than zero, false otherwise
+	 */
 	private boolean checkPlayerTwoHealth() {
 		if(enemy.checkPlayerHealth()) return true;
 		
 		return false;
 	}
 	
+	/**
+	 * Displays the player's hand of cards
+	 * 
+	 * @param player The player whose hand should be displayed
+	 */
 	private void displayPlayerHand(PlayerOne player) {
 		for(int i = 0; i < player.getHand().size(); i++) {
 			System.out.print("Card " + (i + 1) + ": " + player.getHand().get(i).getValue());
@@ -58,7 +95,9 @@ public class GameManager {
 		}
 	}
 	
-	
+	/**
+	 * Starts the game loop, managing battles, player health, levels, and interaction with the shop
+	 */
 	public void start() {
 		giveHand();
 		displayPlayerHand(player); //need to format this correctly
@@ -86,7 +125,7 @@ public class GameManager {
 			int result = battle.startBattle();// call battle method 
 			final int WIN = 1;
 			final int LOSE = 0;
-			 do we want to update level and user money inside battle function or out here
+			// do we want to update level and user money inside battle function or out here
 			if (result == WIN) {
 				userLevel++;
 				Shop.Add_Currency(50);
@@ -115,6 +154,11 @@ public class GameManager {
 		
 	}	
 	
+	/**
+	 * Retrieves the singleton instance of the GameManager
+	 * 
+	 * @return The singleton instance of the GameManager
+	 */
 	public static GameManager getInstance() {
 		if (instance == null) {
 			instance = new GameManager();
