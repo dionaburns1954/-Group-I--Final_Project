480ff5cc3f7f7fcee963eab8923e4c529fe0c92f
azfcf <azfcf@umsystem.edu>
2024-05-06 14:37:37 -0500

    Add healing item

--- src/edu/mu/item/HealItem.java
+++ src/edu/mu/item/HealItem.java
@@ -5,8 +5,8 @@ import edu.mu.players.PlayerOne;
 public class HealItem extends Item {
 	int healthToHeal;
 	
-	public HealItem(String name, int price, int healthToHeal) {
-		super(name, price);
+	public HealItem(String name, int healthToHeal) {
+		super(name, calculateCost(healthToHeal));
 		
 		this.healthToHeal = healthToHeal;
 	}
@@ -14,7 +14,10 @@ public class HealItem extends Item {
 	@Override
 	public void applyEffect() {
 		PlayerOne.getPlayer().addHealth(healthToHeal);
-		
+		System.out.println("You healed " + healthToHeal + " HP. You have " + PlayerOne.getPlayer().getHealth() + " HP.");
 	}
 	
+	private static int calculateCost(int healthToHeal) {
+		return (int) Math.round(Math.pow(1.5, healthToHeal / 10) + 15);
+	}
 }
