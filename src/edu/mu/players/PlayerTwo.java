f5e1c7ea5610076e6eaeca871b3daa56d5019a05
Ryan Esparza <rte0315@gmail.com>
2024-05-05 23:45:33 -0500

    Wrote Javadoc for PlayerTwo

--- src/edu/mu/players/PlayerTwo.java
+++ src/edu/mu/players/PlayerTwo.java
@@ -6,18 +6,33 @@ import edu.mu.card.Card;
 import edu.mu.deck.Deck;
 import edu.mu.deck.EnemyDeck;
 
+/**
+ * Represents Player Two in the game
+ * 
+ * @author Dion Burns
+ */
 public class PlayerTwo extends Player{
 
 	private ArrayList<Card> hand;
 	private EnemyDeck deck;
 	private int health;
 	
+	/**
+	 * Constructs a new PlayerTwo instance
+	 * 
+	 * @param level The level of the enemy
+	 */
 	public PlayerTwo(int level) {
 		//hand = initHand();
 		health = 100;
 		deck = new EnemyDeck(level);
 	}
 	
+	/**
+	 * Refreshes the deck of the player with a new level
+	 * 
+	 * @param lvl The new level
+	 */
 	public void refreshDeck(int lvl) { //for if you want to change difficulty
 		deck.getCards().clear();
 		deck = new EnemyDeck(lvl);
@@ -68,14 +83,29 @@ public class PlayerTwo extends Player{
 		
 	}
 	
+	/**
+	 * Sets the hand of the player
+	 * 
+	 * @param cards The cards to set as the hand
+	 */
 	public void setHand(ArrayList<Card> cards) {
 		hand = cards;
 	}
 	
+	/**
+	 * Gets the hand of the player
+	 * 
+	 * @return The hand of the player
+	 */
 	public ArrayList<Card> getHand() {
 		return hand;
 	}
 	
+	/**
+	 * Gets the deck of the player
+	 * 
+	 * @return The deck of the player
+	 */
 	public Deck getDeck() {
 		return deck;
 	}
