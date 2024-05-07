e7c95d87e7387c4fe909447a4633c7509f935117
azfcf <azfcf@umsystem.edu>
2024-05-06 21:14:39 -0500

    Add Javadoc to SwapCardItem

--- src/edu/mu/item/SwapCardItem.java
+++ src/edu/mu/item/SwapCardItem.java
@@ -9,16 +9,31 @@ import edu.mu.card.Card;
 import edu.mu.players.PlayerOne;
 import edu.mu.players.PlayerTwo;
 
+/**
+ * An item allowing the player to swap a card in their hand with a random card from the enemy's deck.
+ * 
+ * @version 1.0
+ * 
+ * @see Item
+ * 
+ * @author Albert Zhou
+ */
 public class SwapCardItem extends Item {
-
+	// Get the current PlayerTwo instance
 	private PlayerTwo enemy = PlayerTwo.getPlayer();
 	
+	/**
+	 * Parameterized constructor to generate an item with the given name and price.
+	 * @param name The name of the card
+	 * @param price The price of the card
+	 */
 	public SwapCardItem(String name, int price) {
 		super(name, price);
 	}
 	
 	@Override
 	public void applyEffect() {
+		// Get the currentBattleScanner
 		Scanner scanner = Battle.battleScanner;
 		ArrayList<Card> playerHand = PlayerOne.getPlayer().getHand();
 
@@ -26,23 +41,30 @@ public class SwapCardItem extends Item {
 		
 		System.out.println("Select the card you want to swap: ");
 		
+		// Display the player's current hand
 		int i = 0;
 		for(Card card : playerHand) {
 			i++;
 			System.out.println(i + ". " + card.getValue());
 		}
-			
+		
+		// Loop until the player selects a valid card to remove
 		boolean isSelectingCard = true;
 		while(isSelectingCard) {
 			int chosenCardIndex = scanner.nextInt();
 			
+			// If the input is valid, swap the cards
 			if (chosenCardIndex > 0 && chosenCardIndex <= i) {
+				// Get a random index in the enemy's hand
 				int swappedCardIndex = rand.nextInt(enemy.getHand().size() - 1);
 				
 				chosenCardIndex -= 1;
+				
+				// Store the cards
 				Card heldCard = enemy.getHand().get(swappedCardIndex);
 				Card playerCard = playerHand.get(chosenCardIndex);
 				
+				// Swap the cards
 				enemy.getHand().set(swappedCardIndex, playerCard);
 				playerHand.set(chosenCardIndex, heldCard);
 				
